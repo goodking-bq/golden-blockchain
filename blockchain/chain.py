fb554d8b1f0550c23e43becbc12f7d51387bbd92
golden <120225883@qq.com>
2018-02-06 09:30:33 +0800

    chain
    chain

--- blockchain/chain.py
+++ blockchain/chain.py
@@ -9,11 +9,12 @@ class Chain(object):
     """ 链结构"""
 
     def __init__(self, block_cls):
-        self.head = None
+        self.head = None  # 指向最新的block
         self.chain = []
         self.block_cls = block_cls
 
     def new_block(self, block):
         block = self.block_cls.load(block)
+        previous_hash = self.head.hash() if self.head else None
         if self.head == block.index and block.is_valid:
             self.chain.append(block.dict())
